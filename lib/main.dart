de1a082cf7c03358f4c18da092d7c8ea7c1bee72
Simple-Genius <59420341+Simple-Genius@users.noreply.github.com>
2021-08-02 14:06:07 +0000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,23 @@
-import 'package:c19app/prescreens/Onboard.dart';
-import 'package:c19app/prescreens/welcome.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get_storage/get_storage.dart';
+import 'statsPage.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await GetStorage.init();
-  runApp(MyApp());
+
+
+void main() {
+  runApp(MyApp(
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final introdate = GetStorage();
-
-  @override
-  void initState() {
-    super.initState();
-
-    introdate.writeIfNull("displayed", false);
-  }
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //If the it reads true meaning the onboarding page has been showed before show the Welcome page ; if it says false
-      // show the onboarding screen
-      home: introdate.read("displayed") ? Splash() : OnBoardingPage(),
-      routes: {
-        '/splash': (context) => Splash(),
-      },
+      home: StatisticsPage(),
     );
   }
 }
