a3920ed48fb52f6109204e4bc39799079bc36442
George Heavenson Jnr. Oti-Adjei <jrgeorge991@outlook.com>
2021-08-09 12:16:08 +0000

    Addition of statistics page

---
+++ lib/Cards.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: file_names,, use_key_in_widget_constructors, prefer_const_constructors, prefer_typing_uninitialized_variables
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewCards extends StatelessWidget {
+  final Color cardColor;
+  final String topText;
+  var bottomText;
+  final double cardHeight;
+  final double cardWidth;
+
+  NewCards(this.cardColor, this.topText, this.bottomText, this.cardHeight,
+      this.cardWidth);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: cardHeight,
+        width: cardWidth,
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 6.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 6.0),
+                child: Text(
+                  topText,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                '$bottomText',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ));
+  }
+}
